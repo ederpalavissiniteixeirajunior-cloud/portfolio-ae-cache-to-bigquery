069ca16229a3e06ccedf96f61e5cd4ad9e97af5f
Eder Palavissini Teixeira Junior <ederpalavissiniteixeirajunior@gmail.com>
2026-06-13 16:03:33 +0000

    fix: adjusting nomenclature and data tests

--- dbt_project/models/marts/facts/fct_order_items.sql
+++ dbt_project/models/marts/facts/fct_order_items.sql
@@ -39,65 +39,64 @@ dim_calendar as (
 final_join as (
     select
         -- Surrogate Key
-        i.sk_order_item,
+        i.sk_order_item, 
         
         -- Dimension Surrogate Keys
-        cal.sk_time_version,
-        c.sk_customer_version,
-        r.sk_sales_representative_version,
-        col.sk_collection_version,
-        p.sk_product_version,
+        cal.sk_time_version, 
+        c.sk_customer_version, 
+        r.sk_sales_representative_version, 
+        col.sk_collection_version, 
+        p.sk_product_version, 
         
         -- Degenerate Dimensions
-        i.cd_order,
-        i.cd_product,
-        h.cd_customer,
-        h.cd_sales_representative,
-        i.cd_company,
-        h.nm_status,
+        i.cd_order, 
+        i.cd_product, 
+        h.cd_customer, 
+        h.cd_sales_representative, 
+        i.cd_company, 
         
         -- Dates
-        h.dt_issued,
+        h.dt_issued, 
         
         -- Item Quantities
-        i.qt_ordered,
-        i.qt_fulfilled,
-        i.qt_blocked,
-        i.qt_canceled,
+        i.qt_ordered, 
+        i.qt_fulfilled, 
+        i.qt_blocked, 
+        i.qt_canceled, 
         
         -- Item Unit Prices
-        i.vl_unit_price,
-        i.vl_net_unit_price,
-        i.vl_unit_price_me,
+        i.vl_unit_price, 
+        i.vl_net_unit_price, 
+        i.vl_unit_price_me, 
         
         -- Item Total Financial Amounts
-        i.vl_original_amount,
-        i.vl_billed_amount,
-        i.vl_canceled_amount,
-        i.vl_balance_amount,
+        i.vl_original_amount, 
+        i.vl_billed_amount, 
+        i.vl_canceled_amount, 
+        i.vl_balance_amount, 
         i.vl_total_me,
         
-        current_timestamp() as updated_at
+        current_timestamp() as updated_at 
 
     from item_sales i
    
     inner join order_header h 
         on i.cd_order = h.cd_order
         
-    left join dim_customers c 
-        on h.cd_customer = c.cd_customer 
-        and h.dt_issued between c.dbt_valid_from and coalesce(c.dbt_valid_to, '9999-12-31')
-        
-    left join dim_representatives r 
+    left join dim_customers c
+        on h.cd_customer = c.cd_customer
+        and h.dt_issued between c.valid_from and coalesce(c.valid_to, '9999-12-31')
+
+    left join dim_representatives r
         on h.cd_sales_representative = r.cd_sales_representative
-        and h.dt_issued between r.dbt_valid_from and coalesce(r.dbt_valid_to, '9999-12-31')
+        and h.dt_issued between r.valid_from and coalesce(r.valid_to, '9999-12-31')
         
     left join dim_collections col 
         on h.id_collection = col.id_collection
         
-    left join dim_products p 
+    left join dim_products p
         on i.cd_product = p.cd_product
-        and h.dt_issued between p.dbt_valid_from and coalesce(p.dbt_valid_to, '9999-12-31')
+        and h.dt_issued between p.valid_from and coalesce(p.valid_to, '9999-12-31')
         
     left join dim_calendar cal 
         on h.dt_issued = cal.dt_date
--- dbt_project/models/marts/facts/fct_orders.sql
+++ dbt_project/models/marts/facts/fct_orders.sql
@@ -43,13 +43,13 @@ final_join as (
 
     from orders f
     
-    left join dim_customers c 
-        on f.cd_customer = c.cd_customer 
-        and f.dt_issued between c.dbt_valid_from and coalesce(c.dbt_valid_to, '9999-12-31')
-    
-    left join dim_representatives r 
+    left join dim_customers c
+        on f.cd_customer = c.cd_customer
+        and f.dt_issued between c.valid_from and coalesce(c.valid_to, '9999-12-31')
+
+    left join dim_representatives r
         on f.cd_sales_representative = r.cd_sales_representative
-        and f.dt_issued between r.dbt_valid_from and coalesce(r.dbt_valid_to, '9999-12-31')
+        and f.dt_issued between r.valid_from and coalesce(r.valid_to, '9999-12-31')
     
     left join dim_collections col 
         on f.id_collection = col.id_collection
